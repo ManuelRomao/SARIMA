ef819c4b10974e40cf330714fe00765cc51c5ad4
ManuelRomao <98095709+ManuelRomao@users.noreply.github.com>
2022-10-18 14:13:23 +0100

    Update DownloadAzureImages.py

--- DownloadAzureImages.py
+++ DownloadAzureImages.py
@@ -1,2 +1,11 @@
 # Databricks notebook source
 
+
+This doesn't work for years...
+"Scope with Azure KeyVault must have userAADToken defined!"
+
+Story:
+"https://github.com/DataThirstLtd/azure.databricks.cicd.tools/issues/43"
+
+Impact:
+I'll do it in AWS instead
